733eae42bf89946a5c55193f2e1f9d6c4e603945
nakashi <coffeelike.ncnn@gmail.com>
2020-04-28 02:44:12 +0000

    add column

--- database/migrations/2020_04_28_112325_create_tasks_table.php
+++ database/migrations/2020_04_28_112325_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasklist extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class CreateTasklist extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('content'); 
+            $table->string('content');
             $table->timestamps();
         });
     }
